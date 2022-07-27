700d3bd68d4a2d31813d5ec1b8e44ad39b11cae1
eniko42 <szaboeniko1220@gmail.com>
2022-07-27 12:47:18 +0200

    feat: create Recipe entity

---
+++ src/main/java/com/codecool/hogwarts_potions/model/Recipe.java
@@ -0,0 +1,30 @@
+package com.codecool.hogwarts_potions.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "RECIPE")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    @JsonIgnore
+    private Student brewer;
+
+    @OneToMany(mappedBy = "recipe")
+    private List<Ingredient> ingredients;
+}
