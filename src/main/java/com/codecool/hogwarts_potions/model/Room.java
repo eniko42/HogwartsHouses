9d3ea6afa2990976dbeb0cd4e9b905bfcbe83d0d
eniko42 <szaboeniko1220@gmail.com>
2022-07-27 09:55:44 +0200

    feat: implement Room as entity

--- src/main/java/com/codecool/hogwarts_potions/model/Room.java
+++ src/main/java/com/codecool/hogwarts_potions/model/Room.java
@@ -1,20 +1,50 @@
 package com.codecool.hogwarts_potions.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "room")
 public class Room {
 
-    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer capacity;
-    private Set<Student> residents;
+
+    @OneToMany(mappedBy = "room", targetEntity = Student.class)
+    private List<Student> residents = new ArrayList<>();
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Student> getResidents() {
+        return residents;
+    }
+
+    public void setResidents(List<Student> residents) {
+        this.residents = residents;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
