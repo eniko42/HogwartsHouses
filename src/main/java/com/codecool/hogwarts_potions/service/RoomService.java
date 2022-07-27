f594372ab86225f16823f8201834058f194f7968
eniko42 <szaboeniko1220@gmail.com>
2022-07-27 09:59:28 +0200

    feat: wired roomdao to roomservice

--- src/main/java/com/codecool/hogwarts_potions/service/RoomService.java
+++ src/main/java/com/codecool/hogwarts_potions/service/RoomService.java
@@ -1,12 +1,22 @@
 package com.codecool.hogwarts_potions.service;
 
 import com.codecool.hogwarts_potions.model.Room;
+import com.codecool.hogwarts_potions.model.Student;
+import com.codecool.hogwarts_potions.service.repositories.RoomDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RoomService {
+
+    private RoomDao roomDao;
+
+    @Autowired
+    public RoomService(RoomDao roomDao) {
+        this.roomDao = roomDao;
+    }
 
     public List<Room> getAllRooms() {
         //TODO
