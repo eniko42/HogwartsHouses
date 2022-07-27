021c881cbc904b1bafe57e8bbaf750e592990810
eniko42 <szaboeniko1220@gmail.com>
2022-07-27 09:56:19 +0200

    feat: implement Student as entity

--- src/main/java/com/codecool/hogwarts_potions/model/Student.java
+++ src/main/java/com/codecool/hogwarts_potions/model/Student.java
@@ -3,16 +3,67 @@ package com.codecool.hogwarts_potions.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "STUDENT")
 public class Student {
 
-    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private HouseType houseType;
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    @JsonIgnore
+    private Room room;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HouseType getHouseType() {
+        return houseType;
+    }
+
+    public void setHouseType(HouseType houseType) {
+        this.houseType = houseType;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
