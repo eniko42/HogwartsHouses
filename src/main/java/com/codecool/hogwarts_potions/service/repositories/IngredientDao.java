1098b41d5c11c173ffb5d6337fb50a7b1d4d5b20
eniko42 <szaboeniko1220@gmail.com>
2022-07-27 11:59:27 +0200

    feat: create Ingredient repository

---
+++ src/main/java/com/codecool/hogwarts_potions/service/repositories/IngredientDao.java
@@ -0,0 +1,7 @@
+package com.codecool.hogwarts_potions.service.repositories;
+
+import com.codecool.hogwarts_potions.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientDao extends JpaRepository<Ingredient, Long> {
+}
