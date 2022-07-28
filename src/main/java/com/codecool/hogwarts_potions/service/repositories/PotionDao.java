0520e8bd209531c980bfb98e24bd4fe26e43c3f3
eniko42 <szaboeniko1220@gmail.com>
2022-07-28 13:02:28 +0200

    feat: add findByBrewer_Id method

--- src/main/java/com/codecool/hogwarts_potions/service/repositories/PotionDao.java
+++ src/main/java/com/codecool/hogwarts_potions/service/repositories/PotionDao.java
@@ -3,5 +3,8 @@ package com.codecool.hogwarts_potions.service.repositories;
 import com.codecool.hogwarts_potions.model.Potion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PotionDao extends JpaRepository<Potion, Long> {
+    List<Potion> findByBrewer_Id(Long id);
 }
