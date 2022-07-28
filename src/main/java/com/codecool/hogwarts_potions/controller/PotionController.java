e82e9095eabfab4a8c354b5033d8f89c232654c1
eniko42 <szaboeniko1220@gmail.com>
2022-07-28 14:46:19 +0200

    feat: create getRecipesByIngredients route

--- src/main/java/com/codecool/hogwarts_potions/controller/PotionController.java
+++ src/main/java/com/codecool/hogwarts_potions/controller/PotionController.java
@@ -2,6 +2,7 @@ package com.codecool.hogwarts_potions.controller;
 
 import com.codecool.hogwarts_potions.model.Ingredient;
 import com.codecool.hogwarts_potions.model.Potion;
+import com.codecool.hogwarts_potions.model.Recipe;
 import com.codecool.hogwarts_potions.service.PotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -39,4 +40,8 @@ public class PotionController {
         return potionService.updatePotion(ingredient, potion_id);
     }
 
+    @GetMapping("/potions/{potion-id}/help")
+    public List<Recipe> getRecipesByIngredients(@PathVariable("potion-id") Long potion_id) {
+        return potionService.getRecipesByIngredients(potion_id);
+    }
 }
