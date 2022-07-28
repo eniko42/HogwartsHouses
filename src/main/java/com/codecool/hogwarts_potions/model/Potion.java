9760fcba1eaf0d57426dc29d4223a8898669e4a6
eniko42 <szaboeniko1220@gmail.com>
2022-07-28 12:38:48 +0200

    feat: create Potion entity

---
+++ src/main/java/com/codecool/hogwarts_potions/model/Potion.java
@@ -0,0 +1,34 @@
+package com.codecool.hogwarts_potions.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "POTION")
+public class Potion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "brewer_id")
+    @JsonIgnore
+    private Student brewer;
+
+    @ManyToMany
+    private List<Ingredient> ingredients;
+
+    private BrewingStatus status;
+
+    @OneToOne
+    private Recipe recipe;
+}
